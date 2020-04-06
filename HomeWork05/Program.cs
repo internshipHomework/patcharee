a937dbff4942b21158d812851c136aa241e3b915
Your Name <you@example.com>
2020-04-06 12:38:05 +0700

    homework05

--- HomeWork05/Program.cs
+++ HomeWork05/Program.cs
@@ -1,32 +1,59 @@
 ﻿using System;
 
-namespace demo
+public interface IHomework1
 {
-    class Program
+    string DisplayLEDOnScreen(string ledNo);
+}
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            string[] array01 = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "A" };
-            string[] array02= new string[] { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" };
-            int count = 1;
-            while(count <= 200){
-                Console.WriteLine("Please choose LED to turn On/Off : ");
-                string LED = Console.ReadLine();
-                for (int i = 0; i < array01.Length; i++) {
-                    if (array01[i] == LED & array02[i] == "[ ]"){
-                        array02[i] = "[!]"; 
-                    }
-                    else if (array01[i] == LED & array02[i] == "[!]"){
-                        array02[i] = "[ ]" ;
-                    }
-                }
-                Console.WriteLine(string.Join(" ", array02 ) );
-                Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
-                count ++ ;
-            
-
-            }
-        }  
+        LEDclass Led = new LEDclass();
+        Led.DisplayLEDOnScreen(" ");
+        Console.ReadLine();
     }
 }
+class LEDclass : IHomework1
+{
+    public string DisplayLEDOnScreen(string ledNo)
+    {
+        string DisplayLEDOnScreen;
+        DisplayLEDOnScreen = ledNo;
+        string[] array01 = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "A" };
+        string[] array02= new string[] { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" };
+        int count = 1;
+        while(count <= 200){
+            Console.WriteLine(string.Join(" ", array02 ) );
+            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+            count ++ ;
+            Console.WriteLine("Please choose LED to turn On/Off : ");
+
+            string ledno = Console.ReadLine();
+            for (int i = 0; i < array01.Length; i++) {
+                if (array01[i] == ledno & array02[i] == "[ ]"){
+                    array02[i] = "[!]"; 
+                }
+                else if (array01[i] == ledno & array02[i] == "[!]"){
+                    array02[i] = "[ ]" ;
+                }
+            }
+
+        }
+        return ledNo;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
